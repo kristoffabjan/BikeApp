953ea68d2f86e4a6d3cfbc830d5bfceca9ce22c0
kristoffabjan <kristof.fabjan@gmail.com>
2021-06-25 21:49:06 +0200

    links sorting 5th try

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -39,9 +39,9 @@
 
     
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> 
-    <!-- Rates-Carousel  -->
+    <!-- Rates-Carousel  
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> -->
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
